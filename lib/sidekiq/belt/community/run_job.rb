285ab4cbd022cb03e9695397eb14adbbd24491e0
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2024-05-09 23:22:08 -0300

    Format tables to run

--- lib/sidekiq/belt/community/run_job.rb
+++ lib/sidekiq/belt/community/run_job.rb
@@ -6,40 +6,44 @@ module Sidekiq
   module Belt
     module Community
       module RunJob
-        def run
-          args = begin
-            JSON.parse(options)["args"]
-          rescue StandardError
-            {}
-          end
-
-          Module.const_get(klass).perform_async(*args)
-        end
-
         module SidekiqRunJob
           def self.registered(app)
             app.tabs["Run Jobs"] = "run_jobs"
 
             app.get("/run_jobs") do
-              @jobs = [
-                { klass: "AClass", args: [1, 2, 3] }
-              ]
+              @jobs = {}
+
+              jobs = Sidekiq::Config::DEFAULTS.fetch(:run_jobs, [])
+
+              jobs.each_with_index do |job, i|
+                job[:id] = i
+
+                @jobs[job[:group]] ||= []
+                @jobs[job[:group]] << job
+              end
 
               render(:erb, File.read(File.join(__dir__, "views/run_jobs.erb")))
             end
 
             app.post("/run_jobs/:rjid/run") do
+              jobs = Sidekiq::Config::DEFAULTS.fetch(:run_jobs, [])
+              job = jobs[params[:rjid].to_i].symbolize_keys
 
-              #
+              Module.const_get(job[:klass]).perform_async(*job[:args])
+
               return redirect "#{root_path}run_jobs"
             end
           end
         end
 
+        def self.register(jobs)
+          Sidekiq::Config::DEFAULTS[:run_jobs] = jobs
+        end
+
         def self.use!
           require("sidekiq-ent/web")
 
-          Sidekiq::Web.register(Sidekiq::Belt::Community::SidekiqRunJob)
+          Sidekiq::Web.register(Sidekiq::Belt::Community::RunJob::SidekiqRunJob)
         end
       end
     end
