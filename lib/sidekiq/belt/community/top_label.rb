a9b01ac7009a38bcaf42d86982e292ce5200f0cc
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2025-01-10 23:46:23 -0300

    Fix top label

--- lib/sidekiq/belt/community/top_label.rb
+++ lib/sidekiq/belt/community/top_label.rb
@@ -11,14 +11,21 @@ module Sidekiq
           Sidekiq::WebActionHelper.change_layout do |content|
             top_label = (Sidekiq::Belt.config.top_label || {}).fetch(Sidekiq::Belt.env, {})
 
-            html = "<div class='xcontainer-fluid'" \
-                    "style='background: #{::Rack::Utils.escape_html(top_label.fetch(:background_color, "red"))} !important;" \
-                    "position: fixed;z-index: 99999;" \
-                    "text-align: center; color: #{::Rack::Utils.escape_html(top_label.fetch(:color, "white"))};'>" \
-                    "&nbsp;#{::Rack::Utils.escape_html(top_label[:text].to_s)}&nbsp;" \
-                    "</div>"
             unless top_label.empty?
-              content.gsub!('<div class="container-fluid">', "#{html} <div class='container-fluid'>")
+              styles = <<~CSS
+                .top_label {
+                  background: #{::Rack::Utils.escape_html(top_label.fetch(:background_color, "red"))} !important;
+                  z-index: 99999;
+                  text-align: center;
+                  color: #{::Rack::Utils.escape_html(top_label.fetch(:color, "white"))};
+                }
+              CSS
+
+              nonce = content[/nonce="([^"]*)"/, 1].to_s
+              content.gsub!('</head>', "<style nonce='#{nonce}'>#{styles}</style></head>")
+
+              html = "<div class='container-fluid top_label'>&nbsp;#{::Rack::Utils.escape_html(top_label[:text].to_s)}&nbsp;</div>"
+              content.gsub!('<div class="container-fluid">', "#{html}<div class='container-fluid'>")
             end
           end
         end
